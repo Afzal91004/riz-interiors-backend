7de9c8360b7fba750040637d8b4f108a0733195f
Mohammed Afjal Shaikh <afzal91004@gmail.com>
2025-04-25 23:31:39 +0530

    fixes

--- index.js
+++ index.js
@@ -44,3 +44,10 @@ app.listen(port, () => {
 app.use((req, res) => {
   res.status(404).json({ success: false, error: "Route not found" });
 });
+// In your index.js (backend entry point)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
